b5db8d777eacfd3d8dba6d628236aac7e4fb6608
sean3434 <seannyc34@gmail.com>
2022-04-07 16:23:48 -0400

    Fix submit button logic and link to random question

--- app.js
+++ app.js
@@ -11,14 +11,15 @@ let currentAnswer = null
 let questionNumber = 1
 const questionSet = ['A succulent can be any plant with fleshy, thick tissues adapted to store this', 'Baltimore Orioles defensive wizard Brooks Robinson won 16 straight gold these from 1960 to 1975', 'A spy who lures another into a trap, or just a wooden duck', "Chatting at your desk on Instant Messenger? Type AFK into the window to tell them you're away from this", "You might play 301, 501 or 1001 when you're playing when you're having fun playing this barroom favorite", 'This syndrome is also called terror-bonding or traumatic bonding', "It's the type of keyboard named for the succession of 6 letters found near the upper-left corner", "It's not cloudbursts in the skull, but shared problem solving in which everyone contributes ideas", 'If you like it hot, use this brand of sauce made with red peppers, vinegar & salt mined on Avery Island', 'At 16, Tracy Austin beat Chris Evert to win this tennis tournament at Flushing Meadows', 'A type of treeless plain, or the oldest city in Georgia', 'On the night of April 18, 1775 this man rode to Lexington to warn Hancock to get out of town', 'An opening dice throw of 2, 3 or 12 is a loser in this game', 'This unit of measure is equal to 2 pints', 'The olfactory nerve is the nerve of this sense']
 const answerSet = ['water', 'gloves', 'decoy', 'keyboard', 'darts', 'Stockholm', 'QWERTY', 'brainstorming', 'Tabasco', 'U.S. Open', 'Savannah', 'Paul Revere', 'craps', 'quart', 'smell']
+const val = Math.floor(Math.random() * questionSet.length);
+
 //from MDN Math.random documentation to get random integer from 0-14 for questin/answer index
-function getRandomInt(max) {
-    const val = Math.floor(Math.random() * max);
+function getRandomQuestion() {
     currentQuestion = questionSet[val]
     currentAnswer = answerSet[val]
 }
 
-getRandomInt(15)
+getRandomQuestion(15)
 console.log(currentQuestion)
 console.log(currentAnswer)
 
@@ -29,16 +30,16 @@ typeButton.addEventListener('click', () => {
     userInput.style.display = 'inline'
     submitButton.style.display = 'inline'
 })
-// Works but had to copy paste same skip and reveal lines for else statment
-// REVEAL BUTTON ALWAYS SHOWS?
+// Logic for submitting answer, checking if correct, taking away reveal answer button if correct and changing text of next question to skip question
 submitButton.addEventListener('click', () => {
-    if(userInput.value === 'hi') {
+    // if(userInput.value === 'hi') {
+        if(userInput.value === answerSet[val]) {
         submitButton.innerHTML = 'Correct!'
         skipButton.innerHTML = 'Next Question'
         revealButton.style.display = 'none'
-    }else submitButton.innerHTML = 'Incorrect :('
-    skipButton.innerHTML = 'Skip Question'
-    revealButton.style.display = 'inline'
+    }else {submitButton.innerHTML = 'Incorrect :('
+            revealButton.style.display = 'inline'
+}
 })
 
  
