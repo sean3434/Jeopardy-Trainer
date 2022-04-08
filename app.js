241ac57c35341c2474fe017e384e5b9f1f18df32
sean3434 <seannyc34@gmail.com>
2022-04-08 14:38:32 -0400

    Questions display

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const userInput = document.getElementById('my-input')
 const submitButton = document.querySelector('.submit')
 const skipButton = document.querySelector('.skip')
 const revealButton = document.querySelector('.reveal')
+const questions = document.querySelector('.questions')
 
 // Global Variables
 const questionSet = ['A succulent can be any plant with fleshy, thick tissues adapted to store this', 'Baltimore Orioles defensive wizard Brooks Robinson won 16 straight gold these from 1960 to 1975', 'A spy who lures another into a trap, or just a wooden duck', "Chatting at your desk on Instant Messenger? Type AFK into the window to tell them you're away from this", "You might play 301, 501 or 1001 when you're playing when you're having fun playing this barroom favorite", 'This syndrome is also called terror-bonding or traumatic bonding', "It's the type of keyboard named for the succession of 6 letters found near the upper-left corner", "It's not cloudbursts in the skull, but shared problem solving in which everyone contributes ideas", 'If you like it hot, use this brand of sauce made with red peppers, vinegar & salt mined on Avery Island', 'At 16, Tracy Austin beat Chris Evert to win this tennis tournament at Flushing Meadows', 'A type of treeless plain, or the oldest city in Georgia', 'On the night of April 18, 1775 this man rode to Lexington to warn Hancock to get out of town', 'An opening dice throw of 2, 3 or 12 is a loser in this game', 'This unit of measure is equal to 2 pints', 'The olfactory nerve is the nerve of this sense']
@@ -13,6 +14,12 @@ let currentAnswer = null
 let questionNumber = 1
 let NewQuestionId = 0
 
+// Initialize
+newQuestion()
+
+// Display Questions
+questions.innerHTML = currentQuestion
+
 // Get Random Number 0-14 (For question/answer index)
 function newQuestion() {
     newNum = Math.floor(Math.random() * questionSet.length)
@@ -21,7 +28,6 @@ function newQuestion() {
     currentAnswer = answerSet[NewQuestionId]
 }
 
-newQuestion()
 console.log(currentQuestion)
 console.log(currentAnswer)
 
